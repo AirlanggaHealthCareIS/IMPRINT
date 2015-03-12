6949248bda3caa96415c663389cf1732fdb36733
dhexter007 <nur.maulidiyah20@yahoo.co.id>
2015-03-12 21:38:26 +0700

    js

---
+++ assets/www/assets/js/imprint.conf.js
@@ -0,0 +1,28 @@
+/*!
+ * IMPRINT Gocin 0.02.15.1
+ * Copyright 2015 Nur Maulidiyah.
+ * Nur.Maulidiyah20@yahoo.co.id
+ */
+ 
+ /* API SERVICES */
+ var ws_host    = "http://192.168.1.102/API/app_api.php";
+ var ws_timeout	= 10000;
+ 
+ /* GCM SERVICES */
+ var SenderID			= "526266735061";
+ var RegID;
+ var pushNotification;
+ var isreg;
+ var isunreg;
+ var islogin;
+ 
+ /* CAMERA AND FEAT ACCESS */
+  var pictureSource;
+  var destinationType;
+  
+ /* GEOLOCATION*/
+ var isgeoloc;
+ var longitude;
+ var latitude;
+
+ 
